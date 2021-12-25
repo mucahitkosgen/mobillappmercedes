c6df01f4f14ec8f9766f356d3c0065188a3d206b
sarpkan ozcan <sarpkanozcan@sarpkans-MacBook-Pro.local>
2021-12-25 11:52:07 +0300

    firebase->profil-foto baglantisi kuruldu

--- lib/edit_profile.dart
+++ lib/edit_profile.dart
@@ -1,6 +1,5 @@
 // ignore: file_names
 import 'dart:io';
-
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
