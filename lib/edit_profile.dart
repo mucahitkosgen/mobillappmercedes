44601f00a2defdb48bdcb9e528643b8f34929fc1
hasankoroglu <nurdanrseker@gmail.com>
2022-01-06 23:39:34 +0300

    privacyandpolicy

--- lib/edit_profile.dart
+++ lib/edit_profile.dart
@@ -18,14 +18,12 @@ class editProfile extends StatefulWidget {
 }
 
 class _editProfileState extends State<editProfile> {
-
   FirebaseAuth auth = FirebaseAuth.instance;
   late File imageOnScreen;
   String url = "";
   late File _image;
 
-
-  void initState() {
+  /*void initState() {
     super.initState();
     WidgetsBinding.instance!.addPersistentFrameCallback((_) => baglantiAl());
   }
@@ -41,11 +39,11 @@ class _editProfileState extends State<editProfile> {
     setState(() {
       url = baglanti;
     });
-  }
+  }*/
 
-  Future _imgFromCamera() async{
+  Future _imgFromCamera() async {
     var image = await ImagePicker().pickImage(source: ImageSource.camera);
-    setState(() { 
+    setState(() {
       _image = File(image!.path);
     });
 
@@ -53,37 +51,32 @@ class _editProfileState extends State<editProfile> {
         .ref()
         .child("profilePictures")
         .child(auth.currentUser!.uid)
-        .child("profilePicture.png");  
-    
+        .child("profilePicture.png");
 
     UploadTask uploadTask = refPath.putFile(_image);
 
-        uploadTask.whenComplete(() {
-          url = refPath.getDownloadURL() as String;
-        });
-
+    uploadTask.whenComplete(() {
+      url = refPath.getDownloadURL() as String;
+    });
   }
 
   Future _imgFromGallery() async {
-    var image = await  ImagePicker().pickImage(
-        source: ImageSource.gallery
-    );
-      setState(() {
-    _image = File(image!.path);
-  });
+    var image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = File(image!.path);
+    });
 
     Reference refPath = FirebaseStorage.instance
-            .ref()
-            .child("profilePictures")
-            .child(auth.currentUser!.uid)
-            .child("profilePicture.png");  
-    
+        .ref()
+        .child("profilePictures")
+        .child(auth.currentUser!.uid)
+        .child("profilePicture.png");
+
     UploadTask uploadTask = refPath.putFile(_image);
 
-        uploadTask.whenComplete(() {
-          url = refPath.getDownloadURL() as String;
-        });
-
+    uploadTask.whenComplete(() {
+      url = refPath.getDownloadURL() as String;
+    });
   }
 
   late String kullaniciAdi, email, password;
@@ -144,20 +137,19 @@ class _editProfileState extends State<editProfile> {
                         width: 180,
                         height: 180,
                         decoration: BoxDecoration(
-                            border: Border.all(width: 4, color: Colors.white),
-                            boxShadow: [
-                              BoxShadow(
-                                  spreadRadius: 15,
-                                  blurRadius: 20,
-                                  color: Colors.grey.withOpacity(0.1))
-                            ],
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: NetworkImage(url)
-                                // 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/750px-Black_flag.svg.png'
-                            ),
-                          ),
+                          border: Border.all(width: 4, color: Colors.white),
+                          boxShadow: [
+                            BoxShadow(
+                                spreadRadius: 15,
+                                blurRadius: 20,
+                                color: Colors.grey.withOpacity(0.1))
+                          ],
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              fit: BoxFit.cover, image: NetworkImage(url)
+                              // 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/750px-Black_flag.svg.png'
+                              ),
+                        ),
                       ),
                       Positioned(
                         bottom: 0,
@@ -172,8 +164,8 @@ class _editProfileState extends State<editProfile> {
                           child: IconButton(
                             icon: const Icon(
                               Icons.edit,
-                            color: Colors.white,
-                            size: 15,
+                              color: Colors.white,
+                              size: 15,
                             ),
                             onPressed: () => _showPicker(context),
                           ),
@@ -183,35 +175,30 @@ class _editProfileState extends State<editProfile> {
                   ),
                 ),
                 Container(
-                  margin: const EdgeInsets.only(top:25, bottom:0),
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: TextFormField(
-                    style: const TextStyle(color: Colors.white),
-                    onChanged: (String kullaniciAdiTutucu) {
-                      kullaniciAdiAl(kullaniciAdiTutucu);
-                    },
-
-                    decoration: InputDecoration(
+                  margin: const EdgeInsets.only(top: 25, bottom: 0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: TextFormField(
+                      style: const TextStyle(color: Colors.white),
+                      onChanged: (String kullaniciAdiTutucu) {
+                        kullaniciAdiAl(kullaniciAdiTutucu);
+                      },
+                      decoration: InputDecoration(
                         labelText: "Kullanıcı Adı",
                         labelStyle: TextStyle(color: Colors.white),
                         enabledBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.white, width: 2),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(30.0)),
+                          borderSide: BorderSide(color: Colors.white, width: 2),
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
                         ),
                         focusedBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.blue, width: 2),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(30.0)),
+                          borderSide: BorderSide(color: Colors.blue, width: 2),
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
                         ),
                       ),
-                    ),       
+                    ),
                   ),
                 ),
-                
+
                 Padding(
                   padding: const EdgeInsets.all(12.0),
                   child: TextFormField(
@@ -219,24 +206,20 @@ class _editProfileState extends State<editProfile> {
                       emailAl(emailTutucu);
                     },
                     decoration: InputDecoration(
-                        labelText: "Email",
-                        labelStyle: TextStyle(color: Colors.white),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: 
-                                BorderSide(color: Colors.white, width: 2),     
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(30.0)),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.blue, width: 2),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(30.0)),
-                        ),
-                        ),
+                      labelText: "Email",
+                      labelStyle: TextStyle(color: Colors.white),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white, width: 2),
+                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 2),
+                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                      ),
                     ),
                   ),
-                
+                ),
+
                 Padding(
                   padding: const EdgeInsets.all(12.0),
                   child: TextFormField(
@@ -247,37 +230,34 @@ class _editProfileState extends State<editProfile> {
                     decoration: InputDecoration(
                       labelText: "Sifre",
                       labelStyle: TextStyle(color: Colors.white),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: 
-                                BorderSide(color: Colors.white, width: 2),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(30.0)),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.blue, width: 2),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(30.0)),
-                        ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white, width: 2),
+                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 2),
+                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
                       ),
                     ),
                   ),
-                
+                ),
+
                 // ignore: deprecated_member_use
-                Container(  
-                  margin: const EdgeInsets.only(top:25, bottom:0),              
-                child: Center(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: StadiumBorder(),
-                        primary: Colors.blue[900],
-                        onPrimary: Colors.white,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 85, vertical: 20)),
-                    child: Text("Save"),
-                    onPressed: _Save,
+                Container(
+                  margin: const EdgeInsets.only(top: 25, bottom: 0),
+                  child: Center(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: StadiumBorder(),
+                          primary: Colors.blue[900],
+                          onPrimary: Colors.white,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 85, vertical: 20)),
+                      child: Text("Save"),
+                      onPressed: _Save,
+                    ),
                   ),
-                ),)
+                )
               ],
             )),
       ),
@@ -338,34 +318,33 @@ class _editProfileState extends State<editProfile> {
     );
   }
 
-          void _showPicker(context) async {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext bc) {
-                return SafeArea(
-                  child: Container(
-                    child: new Wrap(
-                      children: <Widget>[
-                        new ListTile(
-                            leading: new Icon(Icons.photo_library),
-                            title: new Text('Photo Library'),
-                            onTap: () {
-                              _imgFromGallery();
-                              Navigator.of(context).pop();
-                            }),
-                        new ListTile(
-                          leading: new Icon(Icons.photo_camera),
-                          title: new Text('Camera'),
-                          onTap: () {
-                            _imgFromCamera();
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    ),
+  void _showPicker(context) async {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                  new ListTile(
+                      leading: new Icon(Icons.photo_library),
+                      title: new Text('Photo Library'),
+                      onTap: () {
+                        _imgFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  new ListTile(
+                    leading: new Icon(Icons.photo_camera),
+                    title: new Text('Camera'),
+                    onTap: () {
+                      _imgFromCamera();
+                      Navigator.of(context).pop();
+                    },
                   ),
-                );
-              }
-            );
-          }
+                ],
+              ),
+            ),
+          );
+        });
+  }
 }
